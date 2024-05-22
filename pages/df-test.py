084d37e1257a612a45afd9ae726361e8de2e18c2
Tereza Beránková <xteryna@seznam.cz>
2024-05-22 12:59:47 +0200

    třetí funkční verze - rozšíření analýzy ČR o dispozice - procenta+ piechart

--- pages/df-test.py
+++ pages/df-test.py
@@ -11,18 +11,18 @@ df = df.drop(["Městská část", "Stav", "Číslo inzerátu", "Vlastnictví", "
 df["Cena za m2"] = df["Cena"] / df["Užitná plocha"]
 
 def map_dispozice(dispozice):
-    if ('1+' in dispozice) or ("Garsoniéra" in dispozice):
-        return '1+1 a 1+kk'
-    elif '2+' in dispozice:
-        return '2+1 a 2+kk'
-    elif '3+' in dispozice:
-        return '3+1 a 3+kk'
-    elif '4+' in dispozice:
-        return '4+1 a 4+kk'
+    if ("1+" in dispozice) or ("Garsoniéra" in dispozice):
+        return "1+1/1+kk"
+    elif "2+" in dispozice:
+        return "2+1/2+kk"
+    elif "3+" in dispozice:
+        return "3+1/3+kk"
+    elif "4+" in dispozice:
+        return "4+1/4+kk"
     else:
         return 'Ostatní'
 
-df['Dispozice_kategorie'] = df['Dispozice'].apply(map_dispozice)
+df["Dispozice_kategorie"] = df["Dispozice"].apply(map_dispozice)
 
 if "zobrazit_uvodni_obrazovku" not in st.session_state:
     st.session_state["zobrazit_uvodni_obrazovku"] = True
@@ -43,7 +43,7 @@ if st.session_state["zobrazit_uvodni_obrazovku"]:
         st.experimental_rerun()  # Okamžitě obnoví skript pro zobrazení dalšího obsahu
 else:
     kraje = df["Kraj"].unique().tolist()
-    vybrany_kraj = st.sidebar.selectbox("Vyber kraj, ke kterému se bude vztahovat analýza", kraje, index=0  , key="vybrany_kraj")
+    vybrany_kraj = st.sidebar.selectbox("Vyber kraj, ke kterému se bude vztahovat analýza", kraje, key="vybrany_kraj")
     
     if vybrany_kraj != st.session_state.vybrany_kraj:
         st.session_state.vybrany_kraj = vybrany_kraj
@@ -52,8 +52,6 @@ else:
 
     df_kraj = df[df["Kraj"] == st.session_state.vybrany_kraj]
     okresy = df_kraj["Okres"].unique().tolist()
-
-    st.write(okresy)
     
     if st.session_state.vybrany_okres in okresy:
         vybrany_okres_index = okresy.index(st.session_state.vybrany_okres)
@@ -62,8 +60,9 @@ else:
         if okresy:  # Check if okresy list is not empty
             st.session_state.vybrany_okres = okresy[0]
 
-    vybrany_okres = st.sidebar.selectbox("Vyber okres, ke kterému se bude vztahovat analýza", okresy, index=vybrany_okres_index, key="vybrany_okres")
+    vybrany_okres = st.sidebar.selectbox("Vyber okres, ke kterému se bude vztahovat analýza", okresy, key="vybrany_okres")
 
+    st.write(f"vybraný okres je {vybrany_okres}")
     # Zbytek kódu
 
 
@@ -83,28 +82,24 @@ else:
         return vypocti_vysku
 
     st.title("Analýza bytů k prodeji - Bezrealitky.cz")
+
+    st.write(vybrany_kraj, vybrany_okres)
     #ZALOZKY
     tab1, tab2 =st.tabs(["Analýza v rámci ČR", "Analýza pro vybraný kraj"])
     #ZALOZKA1 - ANALYZA V RAMCI CR
     with tab1:
-        st.subheader("Analýza v rámci ČR - ceny bytů")  
+        st.subheader("Analýza v rámci ČR - ceny bytů")
+        #CENA ZA M2 - ANALÝZA
         df_groupby_kraje_cena_za_m2 = df.groupby("Kraj")[["Užitná plocha", "Cena", "Cena za m2"]].mean().round({"Užitná plocha":0, "Cena": -5,"Cena za m2": -2}).reset_index().sort_values(by= "Cena za m2")
         num_rows = df_groupby_kraje_cena_za_m2.shape[0]
-
         styled_df = df_groupby_kraje_cena_za_m2.style.apply(zvyrazni_radek, axis=1, kategorie="Kraj", vybrana_hodnota=vybrany_kraj)
-
-
+        #výpis df - cena za m2
         st.write("Cena bytů za m2 pro jednotlivé kraje")
         st.dataframe(styled_df, height=(num_rows+1)*35+3, hide_index=True)
-        # #barchart
-        # st.write("Cena bytů za m2 pro jednotlivé kraje")
-        # st.bar_chart(df_groupby_kraje_cena_za_m2.sort_values(by="Cena za m2", ascending=False), x="Kraj", y="Cena za m2")
-
-        #BARCHART PLOTLY
+        #barchart plotly - cena za m2
         sorted_df = df_groupby_kraje_cena_za_m2.sort_values(by="Cena za m2")
-        # Definice barev
         sorted_df["barva"] = sorted_df["Kraj"].apply(lambda x: "lightblue" if x == vybrany_kraj else "blue")
-        # Vytvoření sloupcového grafu pomocí Plotly
+        
         fig = px.bar(sorted_df, y="Kraj", x="Cena za m2", color="barva",
                     color_discrete_map={"lightblue": "lightblue", "blue": "blue"},
                     category_orders={"Kraj": sorted_df["Kraj"].tolist()},
@@ -113,20 +108,19 @@ else:
         st.plotly_chart(fig)
 
         #DISPOZICE ANALÝZA
-        st.subheader("Analýza v rámci ČR - dispozice") 
+        st.subheader("Analýza v rámci ČR - dispozice")
         col1, col2 = st.columns(2)
         with col1:
             df_groupby_dispozice_cena_m2 = df.groupby("Dispozice_kategorie")["Cena za m2"].mean().reset_index().sort_values(by="Cena za m2")
+            #výpis df - dispozice
             st.write("Dispozice bytů - cena za m2")
             num_rows = df_groupby_dispozice_cena_m2.shape[0]
             st.dataframe(df_groupby_dispozice_cena_m2, height=(num_rows+1)*35+3)    
 
         with col2:
-            # st.bar_chart(df_groupby_dispozice_cena_m2)
-
-            #BARCHART PLOTLY
+            #barchart plotly - dispozice, cena za m2
             sorted_df = df_groupby_dispozice_cena_m2.sort_values(by="Cena za m2", ascending=False)
-            # Vytvoření sloupcového grafu pomocí Plotly
+            
             fig = px.bar(sorted_df, y="Dispozice_kategorie", x="Cena za m2",
                         category_orders={"Dispozice_kategorie": sorted_df["Dispozice_kategorie"].tolist()},
                         title="Cena za m2 dle dispozice bytu")
@@ -134,43 +128,50 @@ else:
 
             st.plotly_chart(fig)
 
+        col1, col2 = st.columns(2)
 
-
-
-
-
-        st.write("Nejčetnější dispozice bytů dle krajů")
-        df_groupby_kraje_dispozice = df.groupby(["Kraj", "Dispozice_kategorie"]).size().reset_index(name="Počet")
-        sorted_df_groupby_kraje_dispozice = df_groupby_kraje_dispozice.sort_values(by=["Kraj", "Počet"], ascending=[True, False])
-        result_df = sorted_df_groupby_kraje_dispozice.groupby("Kraj").head(1).reset_index(drop=True)
-        styled_df = result_df.style.apply(zvyrazni_radek, axis=1, kategorie="Kraj", vybrana_hodnota=vybrany_kraj)
-        num_rows = result_df.shape[0]
-        st.dataframe(styled_df, hide_index=True, height=(num_rows+1)*35+3)
+        with col1:
+            #dispozice - četnost
+            st.write("Dispozice bytů - četnost")
+            df_groupby_dispozice = df.groupby("Dispozice_kategorie")["Dispozice_kategorie"].count().reset_index(name="četnost")
+            # Výpočet procentuálního zastoupení výskytů
+            df_groupby_dispozice["procenta"] = (df_groupby_dispozice["četnost"]/df_groupby_dispozice["četnost"].sum()).round(2)
+            df_groupby_dispozice["procenta"] = df_groupby_dispozice["procenta"].map(lambda x: f"{x:.0%}")
+            # Vytvoření pie chartu - dispozice četnost
+            fig = px.pie(df_groupby_dispozice, values="četnost", names="Dispozice_kategorie", title="Dispozice bytů - zastoupení")
+            fig.update_traces(textinfo="percent+label")  # Přidání procentuálních hodnot k jednotlivým segmentům
+            st.plotly_chart(fig, use_container_width=True)
+            
+        with col2:
+            st.write("Nejčetnější dispozice bytů dle krajů")
+            df_groupby_kraje_dispozice = df.groupby(["Kraj", "Dispozice_kategorie"]).size().reset_index(name="Počet")
+            sorted_df_groupby_kraje_dispozice = df_groupby_kraje_dispozice.sort_values(by=["Kraj", "Počet"], ascending=[True, False])
+            result_df = sorted_df_groupby_kraje_dispozice.groupby("Kraj").head(1).reset_index(drop=True)
+            styled_df = result_df.style.apply(zvyrazni_radek, axis=1, kategorie="Kraj", vybrana_hodnota=vybrany_kraj)
+            num_rows = result_df.shape[0]
+            #výpis df - dispozice, četnost
+            st.dataframe(styled_df, hide_index=True, height=(num_rows+1)*35+3)
 
     #ZALOZKA2 - ANALYZA PRO VYBRANY KRAJ
     with tab2:
         st.subheader("Analýza v rámci jednotlivých krajů - ceny bytů")
         df_kraj = df[df["Kraj"]==vybrany_kraj]
 
-        vybrany_okres = "Most"
-        df_groupby_okresy_cena_za_m2 = df.groupby("Okres")[["Užitná plocha", "Cena", "Cena za m2"]].mean().round({"Užitná plocha":0, "Cena": -5,"Cena za m2": -2}).reset_index().sort_values(by= "Cena za m2")
+        #cena za m2
+        df_groupby_okresy_cena_za_m2 = df_kraj.groupby("Okres")[["Užitná plocha", "Cena", "Cena za m2"]].mean().round({"Užitná plocha":0, "Cena": -5,"Cena za m2": -2}).reset_index().sort_values(by= "Cena za m2")
         num_rows = df_groupby_okresy_cena_za_m2.shape[0]
         styled_df = df_groupby_okresy_cena_za_m2.style.apply(zvyrazni_radek, axis=1, kategorie="Okres", vybrana_hodnota=vybrany_okres)
-        st.write("Cena bytů za m2 pro jednotlivé kraje")
-        st.dataframe(styled_df, height=(num_rows+1)*35+3, hide_index=True)
-        #barchart
+        #tabulka - cena za m2
         st.write("Cena bytů za m2 pro jednotlivé okresy")
-        st.bar_chart(df_groupby_okresy_cena_za_m2.sort_values(by="Cena za m2", ascending=False), x="Okres", y="Cena za m2")
-
-        #BARCHART PLOTLY
+        st.dataframe(styled_df, height=(num_rows+1)*35+3, hide_index=True)
+        #barchart plotly - cena za m2
         sorted_df = df_groupby_okresy_cena_za_m2.sort_values(by="Cena za m2")
-        # Definice barev
         sorted_df["barva"] = sorted_df["Okres"].apply(lambda x: "lightblue" if x == vybrany_okres else "blue")
-        # Vytvoření sloupcového grafu pomocí Plotly
+        
         fig = px.bar(sorted_df, y="Okres", x="Cena za m2", color="barva",
                     color_discrete_map={"lightblue": "lightblue", "blue": "blue"},
                     category_orders={"Okres": sorted_df["Okres"].tolist()},
-                    title="Cena za m2 podle kraje")
+                    title="Cena za m2 podle okresu")
         fig.update_layout(showlegend=False)
         st.plotly_chart(fig)
         
